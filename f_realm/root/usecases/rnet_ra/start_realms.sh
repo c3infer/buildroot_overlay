fe828dbd9436ba8901e4fe35ceed02929ad57e79
Amir Al Sadi <alsadi.amir@gmail.com>
2026-02-24 16:14:01 +0000

    patching usecase1

--- f_realm/root/usecases/rnet_ra/start_realms.sh
+++ f_realm/root/usecases/rnet_ra/start_realms.sh
@@ -12,7 +12,7 @@ qemu-system-aarch64\
       -device virtconsole,chardev=virtiocon0 \
       -mon chardev=virtiocon0,mode=readline \
       -kernel /mnt/out/bin/Image-guest \
-      -drive if=none,file=/mnt/out-br/images/rootfs.img,format=raw,id=hd0 \
+      -drive if=none,file=/mnt/out-br/images/rootfs1.img,format=raw,id=hd0 \
       -device virtio-blk-pci,drive=hd0 \
       -object memory-backend-file,size=64M,share=on,mem-path=/dev/shm/shm1,id=shm1 \
       -device ivshmem-plain,memdev=shm1,protected=true \
@@ -38,4 +38,3 @@ qemu-system-aarch64\
       -cpu host -M virt -enable-kvm -M gic-version=3,its=on \
       -smp 1 -m 512M -nographic \
       -append "console=hvc0" < /dev/hvc2 >/dev/hvc2 &
-
