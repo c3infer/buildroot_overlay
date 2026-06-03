99260555934c59ffa3f625dad8afead6dcda57c0
Amir Al Sadi <alsadi.amir@gmail.com>
2026-06-03 17:10:26 +0000

    adding agent chatbot usecase

---
+++ f_realm/root/usecases/agent/start_realms.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -x
+
+echo "model_chatbot (Realm A) <-> shm1 <-> agent_chatbot (Realm B)"
+
+echo "--------------------model_chatbot (Realm A)------------------------"
+qemu-system-aarch64 \
+      -M confidential-guest-support=rme0 \
+      -object rme-guest,id=rme0,measurement-log=on,measurement-algorithm=sha512 \
+      -nodefaults \
+      -chardev stdio,mux=on,id=virtiocon0,signal=off \
+      -device virtio-serial-pci \
+      -device virtconsole,chardev=virtiocon0 \
+      -mon chardev=virtiocon0,mode=readline \
+      -kernel /mnt/out/bin/Image-guest \
+      -drive if=none,file=/mnt/out-br/images/rootfs1.img,format=raw,id=hd0,snapshot=on \
+      -device virtio-blk-pci,drive=hd0 \
+      -object memory-backend-file,size=64M,share=on,mem-path=/dev/shm/shm1,id=shm1 \
+      -device ivshmem-plain,memdev=shm1,protected=true \
+      -device virtio-net-pci,netdev=net0,romfile= \
+      -netdev user,id=net0 \
+      -cpu host -M virt -enable-kvm -M gic-version=3,its=on \
+      -smp 1 -m 1024M -nographic \
+      -append "console=hvc0 root=/dev/vda1 rw model_chatbot" < /dev/hvc1 >/dev/hvc1 &
+
+echo "--------------------agent_chatbot (Realm B)------------------------"
+qemu-system-aarch64 \
+      -M confidential-guest-support=rme0 \
+      -object rme-guest,id=rme0,measurement-log=on,measurement-algorithm=sha512 \
+      -nodefaults \
+      -chardev stdio,mux=on,id=virtiocon0,signal=off \
+      -device virtio-serial-pci \
+      -device virtconsole,chardev=virtiocon0 \
+      -mon chardev=virtiocon0,mode=readline \
+      -kernel /mnt/out/bin/Image-guest \
+      -drive if=none,file=/mnt/out-br/images/rootfs2.img,format=raw,id=hd0,snapshot=on \
+      -device virtio-blk-pci,drive=hd0 \
+      -object memory-backend-file,size=64M,share=on,mem-path=/dev/shm/shm1,id=shm1 \
+      -device ivshmem-plain,memdev=shm1,protected=true \
+      -cpu host -M virt -enable-kvm -M gic-version=3,its=on \
+      -smp 1 -m 1024M -nographic \
+      -append "console=hvc0 root=/dev/vda1 rw agent_chatbot" < /dev/hvc2 >/dev/hvc2 &
